c3c2d3a7a84f47d1a791d60ddc89b8554e04675d
wn5zdvnvkp-dev <wn5zdvnvkp@privaterelay.appleid.com>
2026-03-03 21:28:56 +0900

    header 구현

---
+++ js/api.js
@@ -0,0 +1,16 @@
+const BASE_URL = 'https://dev.wenivops.co.kr/services/fastapi-crud/1';
+
+const getTokens = () => {
+    return localStorage.getItem('access_token');
+};
+
+const getHeaders = () => {
+    const token = getTokens();
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    if (token) {
+        headers['Authorization'] = `Bearer ${token}`;
+    }
+    return headers;
+};
